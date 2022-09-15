8101f4cdaa81e981c5c79963db4f6fb63630c039
lexuankhanh <crazycommander111@gmail.com>
2022-09-15 09:37:42 +0700

    - Add asynchronous feature

--- LocalizedGenerator.py
+++ LocalizedGenerator.py
@@ -1,21 +1,22 @@
 from typing import List
-from googletrans import Translator
 from TranslateContent import LANGUAGES, TranslateContent
 import os.path
 import time
+import asyncio
 
+contents: List[TranslateContent] = []
+iosOutputName = 'output_ios.txt'
+androidOutputName = 'output_android.txt'
+translateLanguages: List[str]= [
+    'id', 'th', 'hi', 'vi', 'es', 'tl',
+    'bn', 'ur', 'zh-tw', 'ja'
+]
+ 
+totalContentToTranslate = 0
+translatedContentCount = 0
+    
 # ========= MAIN =========
-def main():
-    start_time = time.perf_counter()
-    translator = Translator()
-    contents: List[TranslateContent] = []
-    iosOutputName = 'output_ios.txt'
-    androidOutputName = 'output_android.txt'
-    translateLanguages: List[str]= [
-        'id', 'th', 'hi', 'vi', 'es', 'tl',
-        'bn', 'ur', 'zh-tw'
-    ]
-
+async def main():
     myfile = open('input.txt')
     myfileContent = ''
     for line in myfile:
@@ -26,55 +27,60 @@ def main():
         myfileContent = myfileContent + line
     print(myfileContent)
     print()
-    contentToTranslateCount = len(list(filter(lambda x: x.isComment == False,contents)))
+
+    await beginTranslate()
+    writeFile('ios')
+    writeFile('android')
+
+async def beginTranslate():
+    global totalContentToTranslate
+    global translatedContentCount
     
-        
-    iosMode = os.path.exists(iosOutputName) and 'w' or 'x'
-    if iosMode == 'w':
-        outputIOSFile = open(iosOutputName, iosMode)
-        outputIOSFile.close()
-    outputIOSFile = open(iosOutputName, iosMode)
-
-    print('generating for iOS localized')
+    print(f'generating localized')
+    totalContentToTranslate = len(list(filter(lambda x: x.isComment == False,contents))) * len(translateLanguages)
+    translatedContentCount = 0
+    
+    asyncWorks = []
     translatingIndex = 0
     for language in translateLanguages:
         translatingIndex = 0
-        outputIOSFile.write(f'//{LANGUAGES[language]}\n')
         for (index, item) in enumerate(contents):
             if (item.isComment != True):
                 translatingIndex += 1
-                print(f'translating {translatingIndex} / {contentToTranslateCount}', end='\r')
-            result = item.outputTranslateIOS(language=language)
-            outputIOSFile.write(f'{result}\n')
-        print(f'translated all into {LANGUAGES[language]}')
-        outputIOSFile.write('\n')
-    outputIOSFile.close()
-
-    androidMode = os.path.exists(iosOutputName) and 'w' or 'x' 
-    if androidMode == 'w':
-        outputAndroidFile = open(androidOutputName, androidMode)
-        outputAndroidFile.close()
-    outputAndroidFile = open(androidOutputName, androidMode)
-
+            asyncWorks.append(beginTranslateItem(item=item, language=language))
+    await asyncio.gather(*asyncWorks)
     print()
-
-    print('generating for Android localized')
+        
+async def beginTranslateItem(item: TranslateContent, language: str):
+    global translatedContentCount
+    await item.translateAsync(language=language)
+    if (item.isComment != True):
+        translatedContentCount += 1
+        print(f"translated {translatedContentCount}/{totalContentToTranslate}", end='\r')
+    
+def writeFile(platform: str):
+    filePath = iosOutputName if platform == "ios" else androidOutputName
+    permission = os.path.exists(filePath) and 'w' or 'x'
+    if permission == 'w':
+        file = open(filePath, permission)
+        file.close()
+    file = open(filePath, permission)
+    
+    print(f'writing file for {platform} localized')
     translatingIndex = 0
     for language in translateLanguages:
         translatingIndex = 0
-        outputAndroidFile.write(f'//{LANGUAGES[language]}\n')
+        file.write(f'//{LANGUAGES[language]}\n')
         for (index, item) in enumerate(contents):
             if (item.isComment != True):
                 translatingIndex += 1
-                print(f'translating {translatingIndex} / {contentToTranslateCount}', end='\r')
-            result = item.outputTranslateAndroid(language=language)
-            outputAndroidFile.write(f'{result}\n')
-        print(f'translated all into {LANGUAGES[language]}')
-        outputAndroidFile.write('\n')
-    outputAndroidFile.close()
-
-    end_time = time.perf_counter()
-    print(f'executing in {end_time - start_time}s')
-
+            result = item.resultContent(language=language, platform=platform)
+            file.write(f'{result}\n')
+        file.write('\n')
+    file.close()
+        
 if __name__ == '__main__':
-    main()
+    start_time = time.perf_counter()
+    asyncio.run(main())
+    end_time = time.perf_counter()
+    print(f'executed in {end_time - start_time}s')
--- TranslateContent.py
+++ TranslateContent.py
@@ -1,13 +1,19 @@
+import asyncio
+from functools import partial, wraps
 from googletrans import Translator
+import json
 
 class TranslateContent:
     key: str = ''
     content: str = ''
     isComment: bool = False
     translator = Translator()
+    translatedContentsIOS = dict()
+    translatedContentsAndroid = dict()
     
     def __init__(self, line: str):
-        
+        self.translatedContentsIOS = dict()
+        self.translatedContentsAndroid = dict()
         if line.startswith('/*') or line.startswith('//'):
             self.key = line[:-1]
             self.isComment = True
@@ -16,6 +22,12 @@ class TranslateContent:
         parts = line.split(' = ')
         self.key = parts[0][1:].split("\"")[0]
         self.content = parts[1][1:].split("\"")[0]
+        self.translatedContentsIOS['en'] = self.content
+        self.translatedContentsAndroid['en'] = self.content
+       
+        #print(type(self.translatedContents))
+        #print(json.dumps(self.translatedContents, indent = 4))
+        #print(self.translatedContents)
     
     # "log_in" = "Log In"
     def outputTranslateIOS(self, language: str):
@@ -24,15 +36,41 @@ class TranslateContent:
         result = self.translator.translate(self.content, dest=language, src='en')
         return f"\"{self.key}\" = \"{result.text}\";"
     
-    
     # <string name="log_in">Log In</string>
     def outputTranslateAndroid(self, language: str):
         if (self.isComment):
             return self.key
         result = self.translator.translate(self.content, dest=language, src='en')
         return f"<string name=\"{self.key}\">{result.text}</string>"
+        
+    def translate(self, language: str):
+        if (self.isComment):
+            self.translatedContentsIOS[language] = self.key
+            self.translatedContentsAndroid[language] = self.key
+            return
+        result = self.translator.translate(self.content, dest=language, src='en')
+        self.translatedContentsIOS[language] = f"\"{self.key}\" = \"{result.text}\";"
+        self.translatedContentsAndroid[language] = f"<string name=\"{self.key}\">{result.text}</string>"
+        
+    def async_wrap(func):
+        @wraps(func)
+        async def run(*args, loop=None, executor=None, **kwargs):
+            if loop is None:
+                loop = asyncio.get_event_loop()
+            pfunc = partial(func, *args, **kwargs)
+            return await loop.run_in_executor(executor, pfunc)
+        return run 
+        
+    translateAsync = async_wrap(translate)
     
-    
+    def resultContent(self, language: str, platform: str):
+        if (self.isComment):
+            return self.key
+        
+        if (platform == "ios"):
+            return self.translatedContentsIOS[language]
+        
+        return self.translatedContentsAndroid[language]
 
 LANGUAGES = {
     'af': 'afrikaans',
